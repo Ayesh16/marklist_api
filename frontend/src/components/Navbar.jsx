74be3441770c22e343bb6cd58e0a658640b6bf9a
Ayesh <ayeshaayesha16.2003@gmail.com>
2025-02-26 15:35:32 +0530

    UI

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,28 +1,32 @@
-import { FaSignOutAlt } from "react-icons/fa";
+import { FaSignOutAlt, FaBars } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 
-const Navbar = ({ isOpen }) => {
+const Navbar = ({ isOpen, toggleSidebar }) => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
     localStorage.removeItem("role");
-    localStorage.removeItem('token');
-    alert('Logged out successfully!');
-    navigate('/');
+    localStorage.removeItem("token");
+    alert("Logged out successfully!");
+    navigate("/");
   };
 
   return (
     <nav 
-  className={`fixed top-0 bg--300 bg-gradient-to-l from-blue-500 to-blue-100 p-4 flex justify-between items-center  z-50
-    transition-all duration-300 ${isOpen ? "left-64" : "left-16"} right-0`}
->
+      className={`fixed top-0 bg-blue-500 p-4 flex justify-between items-center z-50
+        transition-all duration-300 ${isOpen ? "left-64" : "left-16"} right-0`}
+    >
+      {/* Menu Button */}
+      <button className="p-2" onClick={toggleSidebar}>
+        <FaBars className=" hover:bg-blue-300 text-2xl" />
+      </button>
 
       {/* Logo */}
       <Link to="/" className="text-xl font-bold flex items-center">Marks System</Link>
 
       {/* Logout Button */}
       <button onClick={handleLogout} className="flex items-center space-x-2 bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition">
-        <FaSignOutAlt className="mr-2" />
+        <FaSignOutAlt className="mr-2 " />
         <span>Logout</span>
       </button>
     </nav>
